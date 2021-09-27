a3042b4e6b960600246c39d1da50f0151bb3020c
touros91 <andreageraci91@gmail.com>
2021-09-27 14:07:13 +0200

    Aggiunto il logo e dello stile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// Ciao ragazzi,
-// Esercizio di oggi: Vue Email List
-// nome repo: vue-email-list
-// Descrizione:
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
